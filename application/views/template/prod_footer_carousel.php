c6287dbedb2881b75ee39f5bfda554edcd2372cd
Sudhakar <sudhakar1214@gmail.com>
2012-10-22 18:53:16 +0530

    added css for the tabs

--- application/views/template/prod_footer_carousel.php
+++ application/views/template/prod_footer_carousel.php
@@ -1,9 +1,10 @@
 
     <meta charset="utf-8" />
     <title>jQuery UI Tabs - Content via Ajax</title>
-    
+     <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" /> 
     <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
     <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+	<link rel="stylesheet" href="/resources/demos/style.css" />
     
 	<script>
     $(function() {
