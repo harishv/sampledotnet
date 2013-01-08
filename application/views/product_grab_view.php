0256168bf9033445b022133fa16917000f63c675
Harish Varada <harish.varada@gmail.com>
2013-01-08 23:53:38 +0530

    Fixed the facebook grab url issue

--- application/views/product_grab_view.php
+++ application/views/product_grab_view.php
@@ -1,4 +1,11 @@
 <!-- Begin main-content div -->
 <div id="main-content" class="flt-l wid_100">
-	<iframe src="<?php echo $product_details[0]['grab_url']; ?>" width="100%" height="600px"></iframe>
+	<?php
+		$redirect_url = $product_details[0]['grab_url'];
+
+		if(stristr($redirect_url, 'facebook.com') === FALSE) { ?>
+		<iframe src="<?php echo $redirect_url; ?>" width="100%" height="600px"></iframe>
+	<?php } else {
+			redirect($redirect_url, 'refresh');
+		} ?>
 </div>
